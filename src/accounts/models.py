d306fdb7ff6886a7806fe7f726ebba3c27bb65e6
underscoreanuj <underscoreanuj@gmail.com>
2019-11-04 23:17:21 +0530

    custom user model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser
+)
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email           = models.EmailField(max_length=255, unique=True)
+    full_name       = models.CharField(max_length=255, blank=True, null=True)
+    active          = models.BooleanField(default=True)                  # can login
+    staff           = models.BooleanField(default=False)                 # non-superuser
+    admin           = models.BooleanField(default=False)                 # superuser
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
+
 
 class GuestEmail(models.Model):
     email           = models.EmailField()
